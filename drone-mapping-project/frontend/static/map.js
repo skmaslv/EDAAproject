c9b086e57d9e84ecb528768376eb325bdc7a308e
Saad Kachko <saad.kachko10@gmail.com>
2025-04-04 14:50:58 +0200

    Changes the start point

--- drone-mapping-project/frontend/static/map.js
+++ drone-mapping-project/frontend/static/map.js
@@ -1,6 +1,6 @@
 const markers = [];
 document.addEventListener("DOMContentLoaded", function () {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([55.7068, 13.1870], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
